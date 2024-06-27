9aa45695f78b074111fc6a1e92001ea3f3a2ecd1
Acement <pj.cisterna@gmail.com>
2024-06-26 20:50:41 -0400

    Agregado Arreglo de las normales, su busqueda binaria y reloj

--- gap_coding.cpp
+++ gap_coding.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
-//#include <omp.h>
+#include <random>
+#include <algorithm>
+#include <array>
 
 using namespace std;
 
@@ -26,21 +28,28 @@ int binarysearch(int arreglo[], int l, int r, int num){
 
 int main(){
     
-    int n;
-    int search;
-    int pos;
-    //double t1, t2 , tfin;
+    int n; //Elementos en el array
+    int searchLineal, searchNormal; //Elemento que se esta buscando
+    int pos; //Posicion en la que se encontro el elemento
+    int media = 50;
+    int des_std = 10; //Desviacion estandar
+
+    double t0, t1, t; //Variables para medir tiempo de ejecucion
     
-    //genera arreglos
 
     cout << "Ingrese el numero de elementos: ";
     cin >> n;
 
+    //Semillas
     srand(time(NULL));
+    random_device rd{};
+    mt19937 gen{rd()};
 
-    int num_array[n] = {};
-    int normal_array[n] = {};
 
+    int num_array[n] = {}; //Arreglo lineal
+    int normal_array[n] = {}; //Arreglo normal
+
+    //Agrega n elementos al arreglo lineal
     for (int i = 0; i <= n-1; i++){
         if (i == 0){
             num_array[0] = rand() % 10 + 1;
@@ -48,29 +57,54 @@ int main(){
         else{
             num_array[i] = num_array[i-1] + rand() % 10 + 1;
         }
-        cout << num_array[i];
-        cout << " ";
     }
-
-    cout << "\n\nIngrese numero a buscar: ";
-    cin >> search;
-
-    
+  
     //Busqueda binaria para arreglo lineal
-    //t1 = omp_get_wtime();
-    pos = binarysearch(num_array,0,n-1,search);
-    //t2 = omp_get_wtime();
+
+    cout << "\n\nIngrese numero a buscar en el arreglo lineal: ";
+    cin >> searchLineal;
+
+    t0 = clock();
+    pos = binarysearch(num_array,0,n-1,searchLineal);
+    t1 = clock();
+
+    t = (t1-t0)/CLOCKS_PER_SEC;
 
     if (pos == -1){
-        cout << search  << " no se encuentra en el arreglo lineal" << endl;
+        cout << searchLineal  << " no se encuentra en el arreglo lineal" << endl;
     }
     else{
-        cout << search << " esta en el arreglo lineal, en la posicion: " << pos << endl;
+        cout << searchLineal << " esta en el arreglo lineal, en la posicion: " << pos << endl;
     }
 
-    //tfin = t2 - t1;
+    cout << "Se demoro " << t << " s en hacer la busqueda" << endl;
 
-    //cout << "Se demoro: " << tfin<< " s" << endl;
+
+    //Agrega n elementos al arreglo normal
+    normal_distribution<> distribucion(media, des_std);
+    for (int i = 0; i < n;i++){
+        normal_array[i] = round(distribucion(gen));
+    }
+    sort(normal_array,normal_array + n);
+
+    //Busqueda binaria para el arreglo normal
+    cout << "\n\nIngrese numero a buscar en el arreglo normal: ";
+    cin >> searchNormal;
+
+    t0 = clock();
+    pos = binarysearch(num_array,0,n-1,searchNormal);
+    t1 = clock();
+
+    t = (t1 - t0)/CLOCKS_PER_SEC;
+
+    if (pos == -1){
+        cout << searchNormal  << " no se encuentra en el arreglo normal" << endl;
+    }
+    else{
+        cout << searchNormal << " esta en el arreglo normal, en la posicion: " << pos << endl;
+    }
+
+    cout << "Se demoro " << t << " s en hacer la busqueda" << endl;
 
     return 0;
 }
